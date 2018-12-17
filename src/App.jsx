9b1669b4cefe2535101eeb07c99376b30a393b41
Alyasko <yasko.alexander@gmail.com>
2018-12-17 08:44:20 +0200

    Extracted VisibleTodoList.

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,22 @@
 import React, { Component } from 'react';
-import * as actions from './actions';
-import { addTodo, removeTodo, checkTodo } from './actions';
+import { addTodo } from './actions';
 import { connect } from 'react-redux';
-import TodoList from './TodoList';
 import AddTodo from './AddTodo';
 import Filters from './Filters';
+import VisibleTodoList from './VisibleTodoList';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.deleteTodoHandler.bind(this);
-    this.deleteTodoHandler.bind(this);
-  }
-
   addTodoHandler(text) {
     if (text !== '') {
       this.props.dispatch(addTodo(this.props.todos, text));
     }
   }
 
-  deleteTodoHandler(id) {
-    this.props.dispatch(removeTodo(id));
-  }
-
-  toggleTodoHandler(id) {
-    this.props.dispatch(checkTodo(id));
-  }
-
   render() {
-    const runFilter = (todos, filter) => {
-      switch(filter) {
-        case actions.SHOW_ALL:
-          return todos;
-        case actions.SHOW_ACTIVE: 
-          return todos.filter(x => !x.checked);
-        case actions.SHOW_COMPLETED:
-          return todos.filter(x => x.checked);
-        default:
-          throw new Error("Incorrect filter.");
-      }
-    }
-    const visibleTodos = runFilter(this.props.todos, this.props.filter);
-
     return (
       <div>
         <AddTodo onAddTodo={(text) => this.addTodoHandler(text)} />
-        <TodoList 
-          todos={visibleTodos}
-          onTodoDeleted={(id) => this.deleteTodoHandler(id)}
-          onTodoToggled={(id) => this.toggleTodoHandler(id)}
-        />
+        <VisibleTodoList />
         <Filters />
       </div>
     )
---
+++ src/VisibleTodoList.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import TodoList from './TodoList';
+import { connect } from 'react-redux';
+import * as actions from './actions';
+import { removeTodo, checkTodo } from './actions';
+
+class VisibleTodoList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.deleteTodoHandler.bind(this);
+    this.deleteTodoHandler.bind(this);
+  }
+
+  deleteTodoHandler(id) {
+    this.props.dispatch(removeTodo(id));
+  }
+
+  toggleTodoHandler(id) {
+    this.props.dispatch(checkTodo(id));
+  }
+
+  render() {
+    let { todos, filter } = this.props;
+
+    const runFilter = (todos, filter) => {
+      switch (filter) {
+        case actions.SHOW_ALL:
+          return todos;
+        case actions.SHOW_ACTIVE:
+          return todos.filter(x => !x.checked);
+        case actions.SHOW_COMPLETED:
+          return todos.filter(x => x.checked);
+        default:
+          throw new Error("Incorrect filter.");
+      }
+    }
+    const visibleTodos = runFilter(todos, filter);
+
+    return (
+      <TodoList
+        todos={visibleTodos}
+        onTodoDeleted={(id) => this.deleteTodoHandler(id)}
+        onTodoToggled={(id) => this.toggleTodoHandler(id)}
+      />
+    )
+  }
+}
+
+const mapsStateToProps = (state) => ({
+  todos: state.todos,
+  filter: state.visibilityFilter
+});
+
+export default connect(mapsStateToProps)(VisibleTodoList);
+
